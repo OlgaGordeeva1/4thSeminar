0d670e76e140d8e845819d645c55394a968c8de5
OlgaGord <gordeewa06@gmail.com>
2022-08-24 12:53:32 +0500

    Пример 1 с семинара

---
+++ tst/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+int GetSum(int A)
+{
+    int sum =0;
+    for (int i = 0; i <= A; i++)
+    {
+        sum += i;
+    }
+return sum;
+}
+int a=10;
+int sum = GetSum(a);
+Console.WriteLine($"sum: {sum}");
