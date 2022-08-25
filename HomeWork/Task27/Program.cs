67b233c46403dbcad0a6b96fb354ae6c7f2eed19
OlgaGord <gordeewa06@gmail.com>
2022-08-25 18:15:04 +0500

    Задача 27 решена

--- HomeWork/Task27/Program.cs
+++ HomeWork/Task27/Program.cs
@@ -4,3 +4,22 @@
 // 452 -> 11
 // 82 -> 10
 // 9012 -> 12
+
+
+int Sum(int val)
+{
+    int result = 0;
+    while (val > 0)
+    {
+        int mod = val % 10;
+        result = result + mod;
+        val = val / 10;
+    }
+
+    return result;
+}
+int val = 599;
+
+int result = Sum(val);
+
+Console.Write($"result: {result}");
