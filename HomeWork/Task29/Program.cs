6a37a8e4cf8ca463d89ea043ada7e840f8bcbf98
OlgaGord <gordeewa06@gmail.com>
2022-08-25 18:49:09 +0500

    Задача 29 решена

--- HomeWork/Task29/Program.cs
+++ HomeWork/Task29/Program.cs
@@ -2,4 +2,34 @@
 //  которая задаёт массив из 8 элементов и выводит их на экран.
 
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
-// 6, 1, 33 -> [6, 1, 33
+// 6, 1, 33 -> [6, 1, 33]
+
+int[] NewArr(int n, int min, int max)
+{
+    int[] arr = new int [n];
+    for (int i=0; i<n; i++)
+    {
+        arr[i] = new Random().Next(min,max);
+    }
+return arr;
+}
+
+
+string PrintArr(int[] arr)    
+{
+  string s = String.Empty;
+  int size = arr.Length;
+  for (int i = 0; i < size; i++)
+  {
+    s += Convert.ToString(arr[i]);
+    Console.Write(arr[i]+" ");
+  }
+  return s;
+}
+
+int[] res = NewArr(8, 0, 100);
+Console.Write("s: [");
+string s = PrintArr(res);
+Console.Write("]");
+
+
